5f1e1281767ac95a96bc1eb98f9d80800b40be7c
David Schmidt <66205241+KonsumGandalf@users.noreply.github.com>
2023-07-25 01:09:33 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 sudo apt-get update
 apt-get install -y python3-pip
-pip3 install pytest pytest-cov pytest-json-report requests termcolor colored  
+pip3 install pytest pytest-cov pytest-json-report requests termcolor cs50  
